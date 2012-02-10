dd45208980d63c0a16b5b18fef837746e7609a72
kAmMa <kamma@ubuntu.(none)>
2012-02-10 00:49:13 -0800

    bugfix - FC on press home button

--- src/com/android/contacts/dialpad/adapter/ContactsAdapter.java
+++ src/com/android/contacts/dialpad/adapter/ContactsAdapter.java
@@ -3,10 +3,8 @@ package com.android.contacts.dialpad.adapter;
 import java.util.ArrayList;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.text.SpannableString;
 import android.text.style.BackgroundColorSpan;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,43 +16,43 @@ import com.android.contacts.R;
 import com.android.contacts.dialpad.vo.ContactVO;
 
 public class ContactsAdapter extends ArrayAdapter<ContactVO> {
-  private static final String TAG = "ContactsAdapter";
+	private static final String TAG = "ContactsAdapter";
 
-  private ArrayList<ContactVO> items;
-  private static int MY_BACKGROUD_COLOR;
+	private ArrayList<ContactVO> items;
+	private static int MY_BACKGROUD_COLOR;
 
-  public ContactsAdapter(Context context, int textViewResourceId, ArrayList<ContactVO> items) {
-	super(context, textViewResourceId, items);
-	this.items = items;
-	MY_BACKGROUD_COLOR = this.getContext().getResources().getColor(R.color.call_log_voicemail_highlight_color);
-  }
-
-  @Override
-  public View getView(int position, View convertView, ViewGroup parent) {
-	View v = convertView;
-	if (v == null) {
-	  LayoutInflater vi = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-	  v = vi.inflate(R.layout.dialpad_row, null);
-	}
-	ContactVO contact = items.get(position);
-
-	TextView contactName = (TextView) v.findViewById(R.id.contact_name);
-	if (contactName != null) {
-	  SpannableString text = new SpannableString(contact.getName());
-	  Log.d(TAG, "Setting span for: "+text+" | Start: "+contact.getNameHighlightStart()+" | End:"+contact.getNameHighlightEnd());
-	  if (contact.getNameHighlightStart() >= 0 && contact.getNameHighlightEnd() > contact.getNameHighlightStart())
-		text.setSpan(new BackgroundColorSpan(MY_BACKGROUD_COLOR), contact.getNameHighlightStart(), contact.getNameHighlightEnd(), 0);
-	  contactName.setText(text, BufferType.SPANNABLE);
-	}
-	TextView contactPhone = (TextView) v.findViewById(R.id.contact_phone);
-	if (contactPhone != null) {
-	  SpannableString text = new SpannableString(contact.getPhone() != null ? contact.getPhone() : "");
-	  Log.d(TAG, "Setting span for: "+text+" | Start: "+contact.getPhoneHighlightStart()+" | End:"+contact.getPhoneHighlightEnd());
-	  if (contact.getPhoneHighlightStart() >= 0 && contact.getPhoneHighlightEnd() > contact.getPhoneHighlightStart())
-		text.setSpan(new BackgroundColorSpan(MY_BACKGROUD_COLOR), contact.getPhoneHighlightStart(), contact.getPhoneHighlightEnd(), 0);
-	  contactPhone.setText(text, BufferType.SPANNABLE);
+	public ContactsAdapter(Context context, int textViewResourceId, ArrayList<ContactVO> items) {
+		super(context, textViewResourceId, items);
+		this.items = items;
+		MY_BACKGROUD_COLOR = this.getContext().getResources().getColor(R.color.call_log_voicemail_highlight_color);
 	}
 
-	return v;
-  }
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View v = convertView;
+		if (v == null) {
+			LayoutInflater vi = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			v = vi.inflate(R.layout.dialpad_row, null);
+		}
+		ContactVO contact = items.get(position);
+
+		TextView contactName = (TextView) v.findViewById(R.id.contact_name);
+		if (contactName != null) {
+			SpannableString text = new SpannableString(contact.getName());
+			//Log.d(TAG, "Setting span for: " + text + " | Start: " + contact.getNameHighlightStart() + " | End:" + contact.getNameHighlightEnd());
+			if (contact.getNameHighlightStart() >= 0 && contact.getNameHighlightEnd() > contact.getNameHighlightStart())
+				text.setSpan(new BackgroundColorSpan(MY_BACKGROUD_COLOR), contact.getNameHighlightStart(), contact.getNameHighlightEnd(), 0);
+			contactName.setText(text, BufferType.SPANNABLE);
+		}
+		TextView contactPhone = (TextView) v.findViewById(R.id.contact_phone);
+		if (contactPhone != null) {
+			SpannableString text = new SpannableString(contact.getPhone() != null ? contact.getPhone() : "");
+			//Log.d(TAG, "Setting span for: " + text + " | Start: " + contact.getPhoneHighlightStart() + " | End:" + contact.getPhoneHighlightEnd());
+			if (contact.getPhoneHighlightStart() >= 0 && contact.getPhoneHighlightEnd() > contact.getPhoneHighlightStart())
+				text.setSpan(new BackgroundColorSpan(MY_BACKGROUD_COLOR), contact.getPhoneHighlightStart(), contact.getPhoneHighlightEnd(), 0);
+			contactPhone.setText(text, BufferType.SPANNABLE);
+		}
+
+		return v;
+	}
 }
